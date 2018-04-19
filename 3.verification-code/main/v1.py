a6c95795b32bf26d437a1e19ebf4bfd829334608
Allen2333 <aa704538319@163.com>
2018-04-19 09:41:47 +0800

    4/19/2018

---
+++ 3.verification-code/main/v1.py
@@ -0,0 +1,10 @@
+import random
+checkcode = ''
+for i in range(8):
+    current = random.randrange(0,4)
+    if current != i:
+        temp = chr(random.randint(65,90))
+    else:
+        temp = random.randint(0,9)
+    checkcode += str(temp)
+print(checkcode)
---
+++ test.py
@@ -0,0 +1,12 @@
+import requests
+import re
+content = requests.get('https://book.douban.com/').text
+pattern = re.compile('<li.*?cover.*?href="(.*?)".*?title="(.*?)".*?more-meta.*?author">(.*?)</span>.*?year">(.*?)</span>.*?</li>', re.S)
+results = re.findall(pattern, content)
+print(results)
+
+for result in results:
+    url,name,author,date = result
+    author = re.sub('\s','',author)
+    date = re.sub('\s','',date)
+    print(url,name,author,date)
