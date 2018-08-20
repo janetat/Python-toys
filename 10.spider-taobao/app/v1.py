761c3fd886d5552271c8e5e7ef4f401a0c0d3809
allin <aa704538319@163.com>
2018-08-20 08:10:57 +0800

    修复无法递归爬取

--- 10.spider-taobao/app/v1.py
+++ 10.spider-taobao/app/v1.py
@@ -23,7 +23,8 @@ def index_page(page):
         if page > 1:
             # input为页码输入框
             input = wait.until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, '#mainsrp-pager div.form > input.input J_Input')))
+                #EC.presence_of_element_located((By.CSS_SELECTOR, '#mainsrp-pager div.form > input.input J_Input')))
+                EC.presence_of_element_located((By.CSS_SELECTOR, '#mainsrp-pager div.form > input')))
             # submit为页码提交按钮
             submit = wait.until(
                 EC.element_to_be_clickable((By.CSS_SELECTOR, '#mainsrp-pager div.form > span.btn.J_Submit')))
@@ -40,6 +41,31 @@ def index_page(page):
         get_products()
     except TimeoutException:
         index_page(page)
+
+def index_page(page):
+    """
+    抓取索引页
+    :param page: 页码
+    """
+    print('正在爬取第', page, '页')
+    try:
+        url = 'https://s.taobao.com/search?q=' + quote(KEYWORD)
+        browser.get(url)
+        if page > 1:
+            input = wait.until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, '#mainsrp-pager div.form > input')))
+            submit = wait.until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, '#mainsrp-pager div.form > span.btn.J_Submit')))
+            input.clear()
+            input.send_keys(page)
+            submit.click()
+        wait.until(
+            EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#mainsrp-pager li.item.active > span'), str(page)))
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.m-itemlist .items .item')))
+        get_products()
+    except TimeoutException:
+        index_page(page)
+
 
 
 def get_products():
